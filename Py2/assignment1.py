7dc10e10699993a78efb4131ab6b5a794546a461
WilliamWamalwa <williamwamalwa90@gmail.com>
2025-02-06 11:11:48 +0300

    first commit

---
+++ Py2/assignment1.py
@@ -0,0 +1,12 @@
+yr=int(input("Enter Year:"))
+
+if yr%4==0 :
+    print("leap year")
+else:
+    print("not leap year")
+
+letter=input("Enter a letter:")
+if letter in "a,e,i,o,u":
+    print("vowel")
+else:
+    print("consonant")
